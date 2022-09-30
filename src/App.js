538d46a21d6605a779c707f67b9aff9eb541f494
alfredo lopez lopez <alfredolopez@MacBook-Air-de-alfredo.local>
2022-09-30 14:39:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat/lib/MessengerCustomerChat';
 
 function App() {
   return (
     <div className="App">
+      <MessengerCustomerChat
+        pageId="1031956140334728"
+        appId="617549996573242"
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
